25a621172c5586c184e89b32079244da25b666dd
Can <yamahasirossi@gmail.com>
2022-10-07 21:09:10 +0300

    initial commit

---
+++ src/Core/Models/Entity.ts
@@ -0,0 +1,33 @@
+import EntityInterface from './EntityInterface';
+import BaseUUIDValueObject from './ValueObjects/BaseUUIDValueObject';
+import NullPropertyException from '../Exceptions/NullPropertyException';
+export default abstract class Entity<T extends BaseUUIDValueObject> implements EntityInterface<T> {
+    private uuid: T
+    private createdAt: Date
+    private updatedAt: Date
+    constructor(uuid: T, createdAt:Date, updatedAt:Date){
+        
+        if(!createdAt) throw new NullPropertyException('created at date')
+        if(!updatedAt) throw new NullPropertyException('updated at date')
+        
+        this.uuid = uuid
+        this.createdAt = createdAt
+        this.updatedAt = updatedAt
+    }
+    getUuid(): T {
+        return this.uuid
+    }
+    getCreatedAt(): Date {
+        return this.createdAt
+    }
+    getUpdatedAt(): Date {
+        return this.updatedAt
+    }
+    isNull(): boolean {
+        return false
+    }
+    isNotNull(): boolean {
+        return true 
+    }
+
+}
