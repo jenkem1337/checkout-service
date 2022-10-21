c31346c2f8d1020ce4f6746d94de03a32b1759ab
Can <yamahasirossi@gmail.com>
2022-10-21 15:16:02 +0300

    added new functionality and refactoring

--- src/Core/Models/Domain Models/Checkout/Checkout.ts
+++ src/Core/Models/Domain Models/Checkout/Checkout.ts
@@ -10,8 +10,10 @@ import CheckoutItemID from '../../ValueObjects/CheckoutItemID';
 import CheckoutItemNotFoundException from '../../../Exceptions/CheckoutItemNotFoundException';
 import ItemDeleted from "./Events/ItemDeleted";
 import AnItemDeleted from "./Events/AnItemDeleted";
+import ProductQuantity from '../../ValueObjects/ProductQuantity';
+import CheckoutInterface from './CheckoutInterface';
 
-export default class Checkout extends AggregateRootEntity<CheckoutID> {
+export default class Checkout extends AggregateRootEntity<CheckoutID> implements CheckoutInterface {
     private userUuid: CustomerID
     private address: Address
     private subTotal: Money
@@ -37,33 +39,41 @@ export default class Checkout extends AggregateRootEntity<CheckoutID> {
             this.paymentMethod = paymentMethod
             this.checkoutItems = new Map<string, CheckoutItemInterface>()
         }
+
     addAnItem(item:CheckoutItemInterface): void{
-        this.increaseItemQuantityIfExist(item)
-        this.checkoutItems.set(item.getUuid().getUuid(), item)
-        this.apply(new ItemAdded(item))
-    }
-    private increaseItemQuantityIfExist(item:CheckoutItemInterface): void {
-        if( this.checkoutItems.has(item.getUuid().getUuid()) ) {
+        if(this.increaseItemQuantityIfExist(item)) {
             const itemDomainModel:CheckoutItemInterface = this.checkoutItems.get(item.getUuid().getUuid()) 
             itemDomainModel.incraseQuantity(1)
             this.checkoutItems.set(item.getUuid().getUuid(), itemDomainModel)
+            this.calculateSubTotal()
             this.apply(new ItemAdded(item))
             return;
         }
+        this.checkoutItems.set(item.getUuid().getUuid(), item)
+        this.calculateSubTotal()
+        this.apply(new ItemAdded(item))
     }
-    takeOutAnItemFromList(checkoutItemEntityUuid: CheckoutItemID) {
+    private increaseItemQuantityIfExist(item:CheckoutItemInterface): boolean {
+        return this.checkoutItems.has(item.getUuid().getUuid()) === true
+    }
+
+    takeOutAnItem(checkoutItemEntityUuid: CheckoutItemID) {
         if(this.isNotItemExistInList(checkoutItemEntityUuid)){
             throw new CheckoutItemNotFoundException()
         }
+
         if(this.isOneMoreThanItemExistInList(checkoutItemEntityUuid)) {
             const checkoutItem = this.checkoutItems.get(checkoutItemEntityUuid.getUuid())
             checkoutItem.decreaseQuantity(1)
             this.checkoutItems.set(checkoutItemEntityUuid.getUuid(), checkoutItem)
-            this.apply(new AnItemDeleted(checkoutItemEntityUuid))
+            this.calculateSubTotal()
+            this.apply(new AnItemDeleted(checkoutItemEntityUuid, this.getUuid()))
             return;
         }
+
         this.checkoutItems.delete(checkoutItemEntityUuid.getUuid())
-        this.apply(new ItemDeleted(checkoutItemEntityUuid))
+        this.subTotal = new Money(0)
+        this.apply(new ItemDeleted(checkoutItemEntityUuid, this.getUuid()))
     }
     private isNotItemExistInList(checkoutItemEntityUuid: CheckoutItemID){
         return this.checkoutItems.has(checkoutItemEntityUuid.getUuid()) === false
@@ -73,6 +83,29 @@ export default class Checkout extends AggregateRootEntity<CheckoutID> {
         const checkoutItemQuantity = checkoutItem.getProductQuantity()
         return checkoutItemQuantity.getQuantity() > 1
     }
+
+    calculateSubTotal(){
+        this.subTotal = new Money(0)
+        
+        this.checkoutItems.forEach((item:CheckoutItemInterface, uuid:string) => {
+
+            let itemQuantity: ProductQuantity = item.getProductQuantity()
+            let itemPrice: Money = item.getProductBasePrice()
+            let totalPrice: Money = itemPrice.times(itemQuantity.getQuantity())
+            
+            this.subTotal = this.subTotal.plus(totalPrice.getAmount())
+        })
+        if(this.isSubTotalPriceNotEqualOrMoreThan100()) {
+            this.subTotal = this.subTotal.plus(this.shippingPrice.getAmount())
+        }
+        
+    }
+    private isSubTotalPriceNotEqualOrMoreThan100():boolean{
+        return (this.subTotal.getAmount() >= 100) === false
+    }
+    getSubTotal = () => this.subTotal
+    getUserUuid = () => this.userUuid
+    getAddress  = () => this.address
+    getPeymentMethod = () => this.paymentMethod
     getCheckoutItems = ():Map<string, CheckoutItemInterface> => this.checkoutItems
-    getCheckoutItemsSize = ():number => this.checkoutItems.size
 }
