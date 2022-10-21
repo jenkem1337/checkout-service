95c39473651392b0556defbde919ede09176dfe1
Can <yamahasirossi@gmail.com>
2022-10-21 15:19:25 +0300

    checkoutUuid propery added

--- src/Core/Models/Domain Models/Checkout/Events/AnItemDeleted.ts
+++ src/Core/Models/Domain Models/Checkout/Events/AnItemDeleted.ts
@@ -1,10 +1,13 @@
 import { IEvent } from '@nestjs/cqrs';
 import CheckoutItemID from 'src/Core/Models/ValueObjects/CheckoutItemID';
+import CheckoutID from '../../../ValueObjects/CheckoutID';
 export default class AnItemDeleted implements IEvent {
     
         public checkoutItemEntityUuid:CheckoutItemID
-        constructor (checkoutItemEntityUuid:CheckoutItemID) {
+        public checkoutUuid: CheckoutID
+        constructor (checkoutItemEntityUuid:CheckoutItemID, checkoutUuid: CheckoutID) {
             this.checkoutItemEntityUuid = checkoutItemEntityUuid
+            this.checkoutUuid = checkoutUuid
         }
     
 }
--- src/Core/Models/Domain Models/Checkout/Events/ItemDeleted.ts
+++ src/Core/Models/Domain Models/Checkout/Events/ItemDeleted.ts
@@ -1,9 +1,12 @@
 import { IEvent } from "@nestjs/cqrs";
+import CheckoutID from "../../../ValueObjects/CheckoutID";
 import CheckoutItemID from '../../../ValueObjects/CheckoutItemID';
 
 export default class ItemDeleted implements IEvent {
     public checkoutItemEntityUuid:CheckoutItemID
-    constructor (checkoutItemEntityUuid:CheckoutItemID) {
+    public checkoutUuid: CheckoutID
+    constructor (checkoutItemEntityUuid:CheckoutItemID, checkoutUuid: CheckoutID) {
         this.checkoutItemEntityUuid = checkoutItemEntityUuid
+        this.checkoutUuid = checkoutUuid
     }
 }
