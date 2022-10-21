f77df3dc1e700f17a70c864e7c6544dc3a8614f0
Can <yamahasirossi@gmail.com>
2022-10-21 23:29:33 +0300

    new methods added

--- src/Core/Models/Domain Models/Checkout/CheckoutInterface.ts
+++ src/Core/Models/Domain Models/Checkout/CheckoutInterface.ts
@@ -6,9 +6,14 @@ import Money from '../../ValueObjects/Money';
 import CustomerID from '../../ValueObjects/CustomerID';
 import Address from '../../ValueObjects/Address';
 import PeymentMethod from '../../ValueObjects/PeymentMethod';
+import ProductQuantity from '../../ValueObjects/ProductQuantity';
+import ProductID from '../../ValueObjects/ProductID';
 export default interface CheckoutInterface extends EntityInterface<CheckoutID>{
     addAnItem(item: CheckoutItemInterface):void
+    addItemOneMoreThan(itemUuid: CheckoutItemID,itemQuantity:ProductQuantity):void
     takeOutAnItem(uuid:CheckoutItemID):void
+    takeOutOneMoreThanItem(itemUuid:CheckoutItemID, itemQuantity:ProductQuantity):void
+    updateItemPrices(itemUuid: ProductID,newPrices: Money):void
     calculateSubTotal():void
     getSubTotal():Money
     getUserUuid():CustomerID
