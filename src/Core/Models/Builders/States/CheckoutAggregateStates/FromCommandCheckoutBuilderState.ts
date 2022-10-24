218456cd123269b14967b03a247f83b0d5f5b452
Can <yamahasirossi@gmail.com>
2022-10-24 16:19:07 +0300

    initial commit

---
+++ src/Core/Models/Builders/States/CheckoutAggregateStates/FromCommandCheckoutBuilderState.ts
@@ -0,0 +1,58 @@
+import BaseCheckoutBuilderState from './BaseCheckoutBuilderState';
+import CheckoutInterface from '../../../Domain Models/Checkout/CheckoutInterface';
+import CheckoutItemInterface from '../../../Domain Models/Checkout/CheckoutItemInterface';
+import Address from '../../../ValueObjects/Address';
+import CheckoutID from '../../../ValueObjects/CheckoutID';
+import CheckoutState from '../../../ValueObjects/CheckoutState';
+import CustomerID from '../../../ValueObjects/CustomerID';
+import Money from '../../../ValueObjects/Money';
+import PeymentMethod from '../../../ValueObjects/PeymentMethod';
+import Checkout from '../../../Domain Models/Checkout/Checkout';
+import FailedCheckoutBuilderState from './FailedCheckoutBuilderState';
+
+export default class FromCreationalCommandCheckoutBuilderState extends BaseCheckoutBuilderState {
+    checkoutUuid(uuid: () => CheckoutID): void {
+        this.context._uuid = uuid()
+    }
+    userUuid(userUuid: () => CustomerID): void {
+        this.context._userUuid = userUuid()
+    }
+    address(shippingAddress: () => Address): void {
+        this.context._address = shippingAddress()
+    }
+    subTotal(subTotal: () => Money): void {
+        this.context._subTotal = subTotal()
+    }
+    shippingPrice(shippingPrice: () => Money): void {
+        this.context._shippingPrice = shippingPrice()
+    }
+    peymentMethod(peymentMethod: () => PeymentMethod): void {
+        this.context._paymentMethod = peymentMethod()
+    }
+    checkoutState(checkoutState: () => CheckoutState): void {
+        this.context._checkoutState = checkoutState()
+    }
+    checkoutItemsMap(checkoutItems: Map<string, CheckoutItemInterface>): void {
+        this.context._checkoutItemsMap = checkoutItems
+    }
+    createdAt(date: Date): void {
+        this.context._createdAt = date
+    }
+    updatedAt(date: Date): void {
+        this.context._updatedAt = date
+    }
+    build(): CheckoutInterface {
+        return  Checkout.fromCreationalCommand(
+            this.context._uuid,
+            this.context._userUuid,
+            this.context._address,
+            this.context._subTotal,
+            this.context._shippingPrice,
+            this.context._paymentMethod,
+            this.context._checkoutState,
+            this.context._createdAt,
+            this.context._updatedAt,
+        )
+    }
+
+}
---
+++ src/Core/Models/Domain Models/Checkout/NullCheckout.ts
@@ -0,0 +1,78 @@
+import Address from '../../ValueObjects/Address';
+import CheckoutID from '../../ValueObjects/CheckoutID';
+import CheckoutItemID from '../../ValueObjects/CheckoutItemID';
+import CheckoutState from '../../ValueObjects/CheckoutState';
+import CustomerID from '../../ValueObjects/CustomerID';
+import Money from '../../ValueObjects/Money';
+import PeymentMethod from '../../ValueObjects/PeymentMethod';
+import ProductID from '../../ValueObjects/ProductID';
+import ProductQuantity from '../../ValueObjects/ProductQuantity';
+import CheckoutInterface from './CheckoutInterface';
+import CheckoutItemInterface from './CheckoutItemInterface';
+import NullObjectException from '../../../Exceptions/NullObjectException';
+export default class NullCheckout implements CheckoutInterface {
+    addAnItem(item: CheckoutItemInterface): void {
+        throw new NullObjectException
+    }
+    addItemOneMoreThan(itemUuid: CheckoutItemID, itemQuantity: ProductQuantity): void {
+        throw new NullObjectException
+    }
+    takeOutAnItem(uuid: CheckoutItemID): void {
+        throw new NullObjectException
+    }
+    takeOutOneMoreThanItem(itemUuid: CheckoutItemID, itemQuantity: ProductQuantity): void {
+        throw new NullObjectException
+    }
+    takeOutSameItems(itemUuid: CheckoutItemID): void {
+        throw new NullObjectException
+    }
+    updateItemPrices(itemUuid: ProductID, newPrices: Money): void {
+        throw new NullObjectException
+    }
+    calculateSubTotal(): void {
+        throw new NullObjectException
+    }
+    cancelThisCheckout(): void {
+        throw new NullObjectException
+    }
+    completeThisCheckout(): void {
+        throw new NullObjectException
+    }
+    getSubTotal(): Money {
+        throw new NullObjectException
+    }
+    getUserUuid(): CustomerID {
+        throw new NullObjectException
+    }
+    getAddress(): Address {
+        throw new NullObjectException
+    }
+    getShippingPrice(): Money {
+        throw new NullObjectException
+    }
+    getPeymentMethod(): PeymentMethod {
+        throw new NullObjectException
+    }
+    getCheckoutState(): CheckoutState {
+        throw new NullObjectException
+    }
+    getCheckoutItems(): Map<string, CheckoutItemInterface> {
+        throw new NullObjectException
+    }
+    getUuid(): CheckoutID {
+        throw new NullObjectException
+    }
+    getCreatedAt(): Date {
+        throw new NullObjectException
+    }
+    getUpdatedAt(): Date {
+        throw new NullObjectException
+    }
+    isNull(): boolean {
+         return true
+    }
+    isNotNull(): boolean {
+        return false
+    }
+    
+}
