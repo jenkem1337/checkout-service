f22aff35f70175be5b76ff76194e4cf620534be5
Can <yamahasirossi@gmail.com>
2022-11-30 15:17:51 +0300

    removed

--- src/Application/Modules/ORMModule.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from "@nestjs/common";
-import { ORMProviders } from "./OrmProvider";
-
-@Module({
-    providers: [...ORMProviders],
-    exports:   [...ORMProviders]
-})
-export default class ORMModule{}
