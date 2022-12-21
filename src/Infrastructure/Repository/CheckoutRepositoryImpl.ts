f2fe877d41bfbe3944230d44f2277770f522e16e
Can <yamahasirossi@gmail.com>
2022-12-21 23:16:39 +0300

    mapper context added

--- src/Infrastructure/Repository/CheckoutRepositoryImpl.ts
+++ src/Infrastructure/Repository/CheckoutRepositoryImpl.ts
@@ -3,19 +3,29 @@ import { DataSource } from 'typeorm';
 import { Inject, Injectable } from '@nestjs/common';
 import CheckoutRepository from '../../Core/Interfaces/CheckoutRepository';
 import CheckoutInterface from '../../Core/Models/Domain Models/Checkout/CheckoutInterface';
-import CheckoutDataMapper from '../Entity/Checkout';
-import CheckoutAggregateMapper from './Mapper/CheckoutAggregateMapper';
+import CheckoutDataMapper from '../Entity/CheckoutDataMapper';
+import WriteCheckoutAggregateMapper from './Mapper/WriteCheckoutAggregateMapper';
+import CheckoutAggregateMapperContext from './Mapper/CheckoutAggregateMapperContext';
+import CheckoutAggregateMapperStrategy from './Mapper/CheckoutAggregateStrategy';
 
 @Injectable()
 export default class CheckoutRepositoryImpl implements CheckoutRepository{
+    
+    private readonly objectMapperContext: CheckoutAggregateMapperContext
+    
+    private objectMapper: CheckoutAggregateMapperStrategy<CheckoutDataMapper>
+    
     private readonly dataSoruce:DataSource
     constructor(
-        @Inject("DataSource") dataSource:DataSource
+        @Inject("DataSource") dataSource:DataSource,
+        @Inject(CheckoutAggregateMapperContext.name) objectMapperContext
     ){
         this.dataSoruce = dataSource
+        this.objectMapperContext = objectMapperContext
+        this.objectMapper = this.objectMapperContext.getMapperStrategy();
     }
     async saveChanges(checkout: Checkout): Promise<void> {
-        const checkoutDataMapper = CheckoutAggregateMapper.fromAggregateToDataMapper(checkout)
+        const checkoutDataMapper = this.objectMapper.fromAggregateToDataMapper(checkout)
         await this.dataSoruce.manager.save(checkoutDataMapper)
     }
 
@@ -30,7 +40,7 @@ export default class CheckoutRepositoryImpl implements CheckoutRepository{
             }
         })
         
-        return CheckoutAggregateMapper.fromDataMapperToAggregate(_checkoutDataMapper)
+        return this.objectMapper.fromDataMapperToAggregate(_checkoutDataMapper)
 
     }
     async findManyByCustomerUuid(custormerUuid: string): Promise<CheckoutInterface[]> {
@@ -44,7 +54,7 @@ export default class CheckoutRepositoryImpl implements CheckoutRepository{
             }
         })
 
-        return CheckoutAggregateMapper.fromDataMapperArrayToAggrageteArray(_checkoutDataMapper)
+        return this.objectMapper.fromDataMapperArrayToAggrageteArray(_checkoutDataMapper)
 
     }
     async findOneByUuid(_uuid: string): Promise<CheckoutInterface> {
@@ -56,7 +66,7 @@ export default class CheckoutRepositoryImpl implements CheckoutRepository{
                 uuid: _uuid
             }
         })
-        return CheckoutAggregateMapper.fromDataMapperToAggregate(_checkoutDataMapper)
+        return this.objectMapper.fromDataMapperToAggregate(_checkoutDataMapper)
     }
     async findManyByUuid(uuid: string): Promise<Array<CheckoutInterface>> {
         const _checkoutDataMapper = await this.dataSoruce.manager.find(CheckoutDataMapper, {
@@ -68,7 +78,7 @@ export default class CheckoutRepositoryImpl implements CheckoutRepository{
             }
         })
 
-        return CheckoutAggregateMapper.fromDataMapperArrayToAggrageteArray(_checkoutDataMapper)
+        return this.objectMapper.fromDataMapperArrayToAggrageteArray(_checkoutDataMapper)
     }
     
 }
