b32be4e882e2f5aa4473bba5f7a64c92e99f3816
Can <yamahasirossi@gmail.com>
2022-12-23 22:52:01 +0300

    return type changed from void to Promise<void>

--- src/Core/Interfaces/Repository.ts
+++ src/Core/Interfaces/Repository.ts
@@ -2,7 +2,7 @@ import AggregateRootEntity from "../Models/AggregateRootEntity";
 import BaseUUIDValueObject from '../Models/ValueObjects/BaseUUIDValueObject';
 
 export default interface Repository<RT, MRT, T extends AggregateRootEntity<BaseUUIDValueObject>>{
-    saveChanges(aggregateRoot: T):void
+    saveChanges(aggregateRoot: T):Promise<void>
     findOneByUuid(candidateKey:string):RT
     findManyByUuid(candidateKey:string): MRT
 }
