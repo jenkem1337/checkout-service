171447972b0572f94dfd9d9828979bd842243e73
Can <yamahasirossi@gmail.com>
2023-07-04 21:38:52 +0300

    added new handlers and module

--- src/Application/Modules/Handlers/HandlerModule.ts
+++ src/Application/Modules/Handlers/HandlerModule.ts
@@ -3,11 +3,18 @@ import { CqrsModule } from '@nestjs/cqrs';
 import WriteRepositoryModule from '../RepositoryModule/WriteRepositoryModule';
 import AddAnItemToCartCommadHandler from '../../../Core/Services/Commands/CommandHandlers/AddAnItemToCartCommandHandler';
 import AddItemOneMoreThanCommandHandler from '../../../Core/Services/Commands/CommandHandlers/AddItemOneMoreThanCommandHandler';
-const CommandHandlers = [AddAnItemToCartCommadHandler, AddItemOneMoreThanCommandHandler]
+import DomainModelFactoryModule from '../DomainModelFactoryModule';
+import { CheckoutSagas } from '../../../Core/Services/Sagas/CheckoutSagas';
+import TransactionalCommandHandler from '../../../Core/Services/Commands/CommandHandlers/TransactionalCommandHandler';
+const CommandHandlers = [ 
+    TransactionalCommandHandler,
+    AddAnItemToCartCommadHandler,
+    AddItemOneMoreThanCommandHandler]
 
 @Module({
-    imports: [WriteRepositoryModule, CqrsModule],
+    imports: [WriteRepositoryModule, DomainModelFactoryModule, CqrsModule],
     providers: [
+        CheckoutSagas,
         ...CommandHandlers
     ]
 })
