5a3c8206df03e5efbf41f6eda2f6cd1726764cd9
Can <yamahasirossi@gmail.com>
2023-07-10 00:32:48 +0300

    method name changed

--- src/Application/ClientController/CheckoutClientController.ts
+++ src/Application/ClientController/CheckoutClientController.ts
@@ -13,7 +13,7 @@ export default class CheckoutClientController {
     async addItemOneMoreThan(){}
 
     @Post()
-    async getCheckoutUuidOrCreateCheckout(){}
+    async getCheckoutUuid(){}
 
     @Get()
     async getCheckoutByUuid(){}
