59b464d5d8136e05b3642aa5ccebed1b58123252
Can <yamahasirossi@gmail.com>
2023-07-09 23:18:20 +0300

    AppModule changed with CheckoutClientModule

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import {Transport, MicroserviceOptions} from "@nestjs/microservices"
 import 'reflect-metadata';
 import CheckoutMicroserviceModule from './Application/Modules/CheckoutMicroserviceModule/CheckoutMicroserviceModule';
+import CheckoutClientModule from './Application/Modules/CheckoutClientModule/CheckoutClientModule';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(CheckoutClientModule);
   await app.listen(3000);
 
   const app2 = await NestFactory.createMicroservice<MicroserviceOptions>(
