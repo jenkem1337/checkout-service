47c2633ed021ba588d3c5607f6dc18ce21220556
Can <yamahasirossi@gmail.com>
2023-07-09 11:47:11 +0300

    rewrited module provider and exports

--- src/Application/Modules/ORMModule/MongoOrmModule.ts
+++ src/Application/Modules/ORMModule/MongoOrmModule.ts
@@ -1,8 +1,24 @@
 import { Module } from "@nestjs/common";
-import { mongoORMProvider } from "./OrmProvider";
+import { DataSource } from "typeorm";
+import CheckoutDocument from "src/Infrastructure/Documents/CheckoutDocument";
+import CheckoutItemDocument from "src/Infrastructure/Documents/CheckoutItemDocument";
 
 @Module({
-    providers: [...mongoORMProvider],
-    exports: [...mongoORMProvider]
+    providers: [{
+        provide: 'MongoDataSource',
+        useFactory: async () => {
+          const dataSource = new DataSource({
+            type: "mongodb",
+            host: "localhost",
+            port: 27017,
+            database: "checkout_service_read_db",
+            entities: [
+              CheckoutDocument, CheckoutItemDocument
+            ]
+          })
+          return dataSource.initialize()
+        },
+      }],
+    exports: ["MongoDataSource"]
 })
 export default class MongoORMModule {}
--- src/Application/Modules/ORMModule/PostGreDataSourceModule.ts
+++ src/Application/Modules/ORMModule/PostGreDataSourceModule.ts
@@ -1,8 +1,28 @@
 import { Module } from "@nestjs/common";
-import { postGresProvider } from "./OrmProvider";
+import CheckoutDataMapper from "../../../Infrastructure/Entity/CheckoutDataMapper";
+import CheckoutItemDataMapper from "src/Infrastructure/Entity/CheckoutItemDataMapper";
+import { DataSource } from "typeorm";
 
 @Module({
-    providers: [...postGresProvider],
-    exports:   [...postGresProvider]
+    providers: [{
+        provide: 'DataSource',
+        useFactory: async () => {
+          const dataSource = new DataSource({
+            type: 'postgres',
+            host: 'localhost',
+            port: 5432,
+            username: 'postgres',
+            password: 'admin',
+            database: 'checkout_service_write_db',
+            entities: [
+              CheckoutDataMapper, CheckoutItemDataMapper
+            ],
+            synchronize:true
+          });
+    
+          return dataSource.initialize();
+        },
+    }],
+    exports:["DataSource"]
 })
 export default class PostGreDataSourceModule{}
