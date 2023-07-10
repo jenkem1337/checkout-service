2e63340b397132c14a9b81aaf79a88bd66dbe8bc
Can <yamahasirossi@gmail.com>
2023-07-11 02:03:50 +0300

    some refactoring

--- src/Core/Services/Commands/CommandHandlers/TransactionalCommandHandler.ts
+++ src/Core/Services/Commands/CommandHandlers/TransactionalCommandHandler.ts
@@ -2,28 +2,32 @@ import { CommandBus, CommandHandler, ICommand, ICommandHandler } from "@nestjs/c
 import TransactionalCommand from "../Command/TransactionalCommand";
 import { Inject } from "@nestjs/common";
 import { DataSource } from "typeorm";
+import Result from '../../../Models/Result/Result';
+import ErrorResult from '../../../Models/Result/ErrorResult';
+import NullableResult from '../../../Models/Result/NullableResult';
 @CommandHandler(TransactionalCommand)
-export default class TransactionalCommandHandler implements ICommandHandler<TransactionalCommand<ICommand>> {
+export default class TransactionalCommandHandler implements ICommandHandler<TransactionalCommand<ICommand>, Result<any>> {
     constructor(
         @Inject("DataSource")
         private readonly dataSource: DataSource,
         private readonly commandBus: CommandBus
     ){}
-    async execute(command: TransactionalCommand<ICommand>): Promise<any> {
+    async execute(command: TransactionalCommand<ICommand>): Promise<Result<any>> {
         const queryRunner = this.dataSource.createQueryRunner()
-        await queryRunner.connect()
-        await queryRunner.startTransaction()
-
+        let result: Result<any> = new NullableResult()
         try {
-            await this.commandBus.execute(command.command)
+            await queryRunner.connect()
+            await queryRunner.startTransaction()    
+            result = await this.commandBus.execute(command.command)
             await queryRunner.commitTransaction()
             console.log("commitlendi")
         } catch (error) {
             await queryRunner.rollbackTransaction()
+            result = new ErrorResult(error.message)
         } finally {
             await queryRunner.release()
         } 
-
+        return result
     }
     
 }
