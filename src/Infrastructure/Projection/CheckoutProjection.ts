9ec6c0b3c2333fb63886b5b7df5b97d884854e8c
Can <yamahasirossi@gmail.com>
2023-07-10 20:53:11 +0300

    added handleCheckoutCreatedEvent method

--- src/Infrastructure/Projection/CheckoutProjection.ts
+++ src/Infrastructure/Projection/CheckoutProjection.ts
@@ -1,6 +1,24 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Inject } from "@nestjs/common";
 import {EventPattern} from "@nestjs/microservices"
+import CheckoutQueryModel from '../../Core/Models/QueryModels/CheckoutQueryModel';
+import ReadCheckoutRepository from "../../Core/Interfaces/CheckoutReadRepository";
 @Controller()
 export default class CheckoutProjection {
-    
+
+    constructor(
+        @Inject("CheckoutReadRepository")
+        private readonly chekcoutReadRepository: ReadCheckoutRepository
+    ){}
+    @EventPattern("checkout_created")
+    handleCheckoutCreatedEvent(event:any){
+        const checkoutQueryModel = CheckoutQueryModel.valueOf({
+            checkoutState: event.checkoutState.state,
+            createdDate: event.createdAt,
+            customerUuid: event.userUuid.uuid,
+            subTotal: event.subTotal.amount,
+            updatedDate: event.updatedAt,
+            uuid: event.checkoutUuid.uuid,
+        })
+        this.chekcoutReadRepository.save(checkoutQueryModel)
+    }
 }
