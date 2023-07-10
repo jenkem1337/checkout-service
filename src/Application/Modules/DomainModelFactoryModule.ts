cc81a2334e05acaac7f931a1c3b6e64476ba51e0
Can <yamahasirossi@gmail.com>
2023-07-10 15:14:46 +0300

    added new factory method

--- src/Application/Modules/DomainModelFactoryModule.ts
+++ src/Application/Modules/DomainModelFactoryModule.ts
@@ -6,6 +6,7 @@ import ConcreteCheckoutFactory from '../../Core/Models/Factories/Checkout/Concre
 import ConcreateCheckoutItemFactory from '../../Core/Models/Factories/CheckoutItem/ConcreateCheckoutItem';
 import ConcreateAllArgumentCheckoutFactory from '../../Core/Models/Factories/Checkout/ConcreateAllArgumentCheckoutFactory';
 import NullableAllArgumentCheckoutFactory from '../../Core/Models/Factories/Checkout/NullableAllArgumentCheckoutFactory';
+import CreateCheckoutWithCheckoutCreatedEventFactory from "../../Core/Models/Factories/Checkout/CreateCheckoutWithCheckoutCreatedEventFactory";
 
 @Module({
     providers: [{
@@ -19,6 +20,7 @@ import NullableAllArgumentCheckoutFactory from '../../Core/Models/Factories/Chec
                     .addFactoryClass(NullableCheckoutItemFactory.name, new NullableCheckoutItemFactory())
                     .addFactoryClass(ConcreateAllArgumentCheckoutFactory.name, new ConcreateAllArgumentCheckoutFactory())
                     .addFactoryClass(NullableAllArgumentCheckoutFactory.name, new NullableAllArgumentCheckoutFactory())
+                    .addFactoryClass(CreateCheckoutWithCheckoutCreatedEventFactory.name, new CreateCheckoutWithCheckoutCreatedEventFactory())
 
             return factoryCtx
         }
