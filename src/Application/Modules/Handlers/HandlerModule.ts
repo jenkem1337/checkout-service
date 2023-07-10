4cf96cef447b0bfd51b995d3024c780af4443c21
Can <yamahasirossi@gmail.com>
2023-07-10 15:14:59 +0300

    added CreateCheckoutCommandHandler

--- src/Application/Modules/Handlers/HandlerModule.ts
+++ src/Application/Modules/Handlers/HandlerModule.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { CqrsModule } from '@nestjs/cqrs';
+import { CqrsModule, ICommandHandler } from '@nestjs/cqrs';
 import WriteRepositoryModule from '../RepositoryModule/WriteRepositoryModule';
 import AddAnItemToCartCommadHandler from '../../../Core/Services/Commands/CommandHandlers/AddAnItemToCartCommandHandler';
 import AddItemOneMoreThanCommandHandler from '../../../Core/Services/Commands/CommandHandlers/AddItemOneMoreThanCommandHandler';
@@ -7,10 +7,13 @@ import DomainModelFactoryModule from '../DomainModelFactoryModule';
 import { CheckoutSagas } from '../../../Core/Services/Sagas/CheckoutSagas';
 import TransactionalCommandHandler from '../../../Core/Services/Commands/CommandHandlers/TransactionalCommandHandler';
 import PostGreDataSourceModule from '../ORMModule/PostGreDataSourceModule';
+import CreateCheckoutCommandHandler from '../../../Core/Services/Commands/CommandHandlers/CreateCheckoutCommandHandler';
 const CommandHandlers = [ 
     TransactionalCommandHandler,
     AddAnItemToCartCommadHandler,
-    AddItemOneMoreThanCommandHandler]
+    AddItemOneMoreThanCommandHandler,
+    CreateCheckoutCommandHandler
+]
 
 @Module({
     imports: [PostGreDataSourceModule,WriteRepositoryModule, DomainModelFactoryModule, CqrsModule],
