fb2b8eaabf5b2019fb4f94bbbd76b08eb5052642
Can <yamahasirossi@gmail.com>
2023-07-10 16:00:03 +0300

    removed unnecessary method

--- src/Infrastructure/Projection/CheckoutProjection.ts
+++ src/Infrastructure/Projection/CheckoutProjection.ts
@@ -1,9 +1,6 @@
 import { Controller } from "@nestjs/common";
 import {EventPattern} from "@nestjs/microservices"
-import CheckoutCreatedAndOneCheckoutItemAddedEvent from "../../Core/Services/Events/CheckoutCreatedAndOneCheckoutItemAddedEvent";
 @Controller()
 export default class CheckoutProjection {
-    @EventPattern('checkout_created_and_an_item_added')
-    async checkoutCreated(event: CheckoutCreatedAndOneCheckoutItemAddedEvent){}
     
 }
