49ffe397ff8389c0075178213d0dec49b6abb208
Can <yamahasirossi@gmail.com>
2023-07-10 20:54:32 +0300

    createCheckout method added and some code commented

--- src/Application/ClientController/CheckoutClientController.ts
+++ src/Application/ClientController/CheckoutClientController.ts
@@ -1,36 +1,47 @@
-import { Controller, Delete, Get, Post, Put } from "@nestjs/common";
+import { Controller, Delete, Get, Post, Put, Request, UseGuards } from "@nestjs/common";
 import CheckoutClientService from "../ClientService/CheckoutClientService";
+import { JwtAuthGuard } from "../Auth/JwtAuthGuard";
 
 @Controller("/checkout")
 export default class CheckoutClientController {
-    constructor(private readonly checkoutService:CheckoutClientService){}
-    @Post("/insert-an-item")
-    async addAnItemToCheckout(){
-        return this.checkoutService.addAnItemToCheckout()
+    constructor(
+        private readonly checkoutService:CheckoutClientService,
+    ){}
+    
+
+    @Post()
+    async createCheckout(){
+        return this.checkoutService.createCheckout()
     }
 
-    @Post()
-    async addItemOneMoreThan(){}
+    @UseGuards(JwtAuthGuard)
+    @Get("/insert-an-item")
+    async addAnItemToCheckout(@Request() req){
+        this.checkoutService.addAnItemToCheckout()
+    }
 
-    @Post()
-    async getCheckoutUuid(){}
-
-    @Get()
-    async getCheckoutByUuid(){}
-
-    @Get()
-    async getCheckoutByUuidAndCustomerUuid(){}
-
-    @Delete()
-    async deleteAnCheckoutItemByUuid(){}  
-    
-    @Delete()
-    async deleteAllSameItemsByUuid(){}
-
-    @Delete()
-    async deleteItemsOneMoreThanByUuid(){}
-
-    @Post()
-    async createOrder(){}
-
+    //@Post()
+    //async addItemOneMoreThan(){}
+//
+    //@Post()
+    //async getCheckoutUuid(){}
+//
+    //@Get()
+    //async getCheckoutByUuid(){}
+//
+    //@Get()
+    //async getCheckoutByUuidAndCustomerUuid(){}
+//
+    //@Delete()
+    //async deleteAnCheckoutItemByUuid(){}  
+    //
+    //@Delete()
+    //async deleteAllSameItemsByUuid(){}
+//
+    //@Delete()
+    //async deleteItemsOneMoreThanByUuid(){}
+//
+    //@Post()
+    //async createOrder(){}
+//
 }
