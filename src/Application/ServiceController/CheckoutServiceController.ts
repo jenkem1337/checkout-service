91f7778ac0ecfe4516641af15114c78d9be97b9b
Can <yamahasirossi@gmail.com>
2023-07-11 20:28:37 +0300

    findAnCheckoutByUuidAndCustomerUuid implemented

--- src/Application/ServiceController/CheckoutServiceController.ts
+++ src/Application/ServiceController/CheckoutServiceController.ts
@@ -1,13 +1,15 @@
 import { Controller } from "@nestjs/common";
-import { CommandBus } from "@nestjs/cqrs";
+import { CommandBus, QueryBus } from "@nestjs/cqrs";
 import { MessagePattern, Payload } from "@nestjs/microservices";
 import CreateCheckoutCommand from '../../Core/Services/Commands/Command/CreateCheckoutCommand';
 import TransactionalCommand from '../../Core/Services/Commands/Command/TransactionalCommand';
+import CheckoutByUuidAndCustomerUuidQuery from "src/Core/Services/Queries/Query/CheckoutByUuidAndCustomerUuidQuery";
 
 @Controller()
 export default class CheckoutServiceController {
     constructor(
-        private readonly commandBus: CommandBus
+        private readonly commandBus: CommandBus,
+        private readonly queryBus: QueryBus
     ){}
     @MessagePattern({cmd: "create_checkout"})
     async createCheckout(@Payload() uuid:string){
@@ -15,6 +17,12 @@ export default class CheckoutServiceController {
         return await this.commandBus.execute(new TransactionalCommand(
                                         new CreateCheckoutCommand(uuid)
                                     ))
-        
+    }
+
+    @MessagePattern({cmd: "find_an_checkout_by_uuid_and_customer_uuid"})
+    async findAnCheckoutByUuidAndCustomerUuid(@Payload() payload:{checkoutUuid:string, customerUuid:string}){
+        return await this.queryBus.execute(
+            new CheckoutByUuidAndCustomerUuidQuery(payload.checkoutUuid, payload.customerUuid)
+        )
     }
 }
