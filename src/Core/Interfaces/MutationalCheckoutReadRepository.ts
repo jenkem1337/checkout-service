6d0402df1a2fca31254ab1b1850267022e09bccb
Can <yamahasirossi@gmail.com>
2023-07-13 13:20:32 +0300

    added updateStateByUuid method

--- src/Core/Interfaces/MutationalCheckoutReadRepository.ts
+++ src/Core/Interfaces/MutationalCheckoutReadRepository.ts
@@ -5,5 +5,6 @@ export default interface MutationalCheckoutReadRepository {
     save(checkout:CheckoutQueryModel):Promise<void>
     saveCheckoutItem(item: CheckoutItemQueryModel): Promise<void>
     updateByUuid(checkout:CheckoutQueryModel):Promise<void>
+    updateStateByUuid(uuid:string, state:string):Promise<void>
     deleteCheckoutItemByUuid(uuid:string):Promise<void>
 }
