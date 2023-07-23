c6af6a7815bd515464645aaba8ccc7f77aa3103a
Can <yamahasirossi@gmail.com>
2023-07-23 14:14:47 +0300

    added subTotal property

--- src/Core/Models/Domain Models/Checkout/Events/ItemQuantityIncreased.ts
+++ src/Core/Models/Domain Models/Checkout/Events/ItemQuantityIncreased.ts
@@ -2,14 +2,17 @@ import { IEvent } from '@nestjs/cqrs';
 import CheckoutID from '../../../ValueObjects/CheckoutID';
 import CheckoutItemID from '../../../ValueObjects/CheckoutItemID';
 import ProductQuantity from '../../../ValueObjects/ProductQuantity';
+import Money from 'src/Core/Models/ValueObjects/Money';
 export default class ItemQuantityIncreased implements IEvent {
     public checkoutUuid: CheckoutID
     public checkoutItemUuid: CheckoutItemID
     public itemQuantity: ProductQuantity
+    public subTotal: Money
 
-    constructor(checkoutUuid: CheckoutID, checkoutItemUuid: CheckoutItemID, itemQuantity: ProductQuantity){
+    constructor(checkoutUuid: CheckoutID, checkoutItemUuid: CheckoutItemID, itemQuantity: ProductQuantity, subTotal:Money){
             this.checkoutUuid = checkoutUuid
             this.checkoutItemUuid = checkoutItemUuid
             this.itemQuantity = itemQuantity
+            this.subTotal = subTotal
         }
 }
