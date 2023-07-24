b3311ebffbbf6d8508e7b68b804d1b66368848fc
Can <yamahasirossi@gmail.com>
2023-07-24 09:18:51 +0300

    added new properties

--- src/Core/Models/Domain Models/Checkout/Events/ItemDeleted.ts
+++ src/Core/Models/Domain Models/Checkout/Events/ItemDeleted.ts
@@ -1,12 +1,15 @@
 import { IEvent } from "@nestjs/cqrs";
 import CheckoutID from "../../../ValueObjects/CheckoutID";
 import CheckoutItemID from '../../../ValueObjects/CheckoutItemID';
+import Money from "src/Core/Models/ValueObjects/Money";
 
 export default class ItemDeleted implements IEvent {
-    public checkoutItemEntityUuid:CheckoutItemID
+    public checkoutItemUuid:CheckoutItemID
     public checkoutUuid: CheckoutID
-    constructor (checkoutItemEntityUuid:CheckoutItemID, checkoutUuid: CheckoutID) {
-        this.checkoutItemEntityUuid = checkoutItemEntityUuid
+    public subTotal:Money
+    constructor (checkoutItemEntityUuid:CheckoutItemID, checkoutUuid: CheckoutID, subTotal:Money) {
+        this.checkoutItemUuid = checkoutItemEntityUuid
         this.checkoutUuid = checkoutUuid
+        this.subTotal = subTotal
     }
 }
--- src/Core/Models/Domain Models/Checkout/Events/ItemDeletedAsQuantity.ts
+++ src/Core/Models/Domain Models/Checkout/Events/ItemDeletedAsQuantity.ts
@@ -2,13 +2,16 @@ import { IEvent } from '@nestjs/cqrs';
 import CheckoutID from '../../../ValueObjects/CheckoutID';
 import CheckoutItemID from '../../../ValueObjects/CheckoutItemID';
 import ProductQuantity from '../../../ValueObjects/ProductQuantity';
+import Money from 'src/Core/Models/ValueObjects/Money';
 export default class ItemDeletedAsQuantity implements IEvent {
-    public checkoutItemEntityUuid:CheckoutItemID
+    public checkoutItemUuid:CheckoutItemID
     public checkoutUuid: CheckoutID
     public quantity: ProductQuantity
-    constructor (checkoutItemEntityUuid:CheckoutItemID, checkoutUuid: CheckoutID, quantity: ProductQuantity) {
-        this.checkoutItemEntityUuid = checkoutItemEntityUuid
+    public subTotal: Money
+    constructor (checkoutItemEntityUuid:CheckoutItemID, checkoutUuid: CheckoutID, subTotal:Money, quantity: ProductQuantity) {
+        this.checkoutItemUuid = checkoutItemEntityUuid
         this.checkoutUuid = checkoutUuid
+        this.subTotal = subTotal
         this.quantity = quantity
     }
 
