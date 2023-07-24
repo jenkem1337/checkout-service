d5331aefbdc1c3af64151eec3ee3b6be56fba639
Can <yamahasirossi@gmail.com>
2023-07-24 09:17:45 +0300

    added checkoutRepository property and implemented

--- src/Core/Services/Events/EventHandlers/ItemDeletedEventHandler.ts
+++ src/Core/Services/Events/EventHandlers/ItemDeletedEventHandler.ts
@@ -1,16 +1,20 @@
 import { Inject } from "@nestjs/common";
 import { EventsHandler, IEventHandler, InvalidEventsHandlerException } from "@nestjs/cqrs";
 import { ClientProxy } from "@nestjs/microservices";
+import CheckoutRepository from "src/Core/Interfaces/CheckoutRepository";
 import ItemDeleted from "src/Core/Models/Domain Models/Checkout/Events/ItemDeleted";
 
 @EventsHandler(ItemDeleted)
 export default class ItemDeletedEventHandler implements IEventHandler<ItemDeleted>{
     constructor(
         @Inject("CHECKOUT_PROJECTION_SERVICE")
-        private readonly checkoutProjectionClient:ClientProxy
+        private readonly checkoutProjectionClient:ClientProxy,
+        @Inject("CheckoutRepository")
+        private readonly checkoutRepository:CheckoutRepository
     ){}
 
     handle(event: ItemDeleted) {
+        this.checkoutRepository.removeCheckoutItemByUuid(event.checkoutItemUuid.getUuid())
         this.checkoutProjectionClient.emit("item-deleted", event)
     }
 }
