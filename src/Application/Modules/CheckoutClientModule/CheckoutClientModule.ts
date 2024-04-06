3cf73d67091fc27effb388d2b6c7d5deb119b1f7
Can <yamahasirossi@gmail.com>
2024-04-07 00:35:37 +0300

    Update CheckoutClientModule.ts

--- src/Application/Modules/CheckoutClientModule/CheckoutClientModule.ts
+++ src/Application/Modules/CheckoutClientModule/CheckoutClientModule.ts
@@ -1,26 +1,41 @@
-import { Module } from "@nestjs/common";
+import { MiddlewareConsumer, Module, NestModule } from "@nestjs/common";
 import CheckoutClientController from '../../ClientController/CheckoutClientController';
-import CheckoutClientService from '../../ClientService/CheckoutClientService';
-import { ClientsModule, Transport } from "@nestjs/microservices";
 import { JwtModule } from "@nestjs/jwt";
 import { PassportModule } from "@nestjs/passport";
 import JwtStrategy from '../../Auth/JwtAuthPassport';
+import { CqrsModule } from "@nestjs/cqrs";
+import HandlerModule from "../Handlers/HandlerModule";
+import CheckoutService from "../../Service/CheckoutClientService";
+import { AlsModule } from "../AlsModule";
+import { AsyncLocalStorage } from "async_hooks";
+import { QueryRunner } from "typeorm";
+import TransactionManagerFactoryModule from "../RepositoryModule/TransactionManagerFactoryModule";
+
 
 @Module({
     controllers:[CheckoutClientController],
-    providers: [CheckoutClientService, JwtStrategy],
+    providers: [CheckoutService, JwtStrategy],
     imports:[
         JwtModule.register({secret: "$2y$10$YZM.npx0LPKqbmQjzWNTWeqX06MUkm6wtsS2jxdDtasWWXFzNhuES"}),
         PassportModule,
-        ClientsModule.register([{
-            name:"CHECKOUT_SERVICE",
-            transport:Transport.REDIS,
-            options: {
-                host:"localhost",
-                port:6379
-            }
-
-        }])
+        HandlerModule, 
+        CqrsModule,
+        AlsModule,
+        TransactionManagerFactoryModule
     ]
 })
-export default class CheckoutClientModule {}
+export default class CheckoutClientModule implements NestModule{
+    constructor(
+        private readonly als: AsyncLocalStorage<Map<string, QueryRunner>>
+      ) {}
+    
+    configure(consumer: MiddlewareConsumer) {
+        consumer
+        .apply((req, res, next) => {
+          this.als.run(new Map(), () => next());
+        })
+        .forRoutes('*');
+  
+    }
+
+}
