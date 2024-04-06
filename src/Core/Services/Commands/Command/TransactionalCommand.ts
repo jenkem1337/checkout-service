86d6b4af355c6b803e37753f0cc9475738dc352b
Can <yamahasirossi@gmail.com>
2024-04-07 00:32:59 +0300

    deleted

--- src/Core/Services/Commands/Command/TransactionalCommand.ts
+++
@@ -1,7 +0,0 @@
-import { ICommand } from "@nestjs/cqrs";
-
-export default class TransactionalCommand<T extends ICommand> implements ICommand {
-    constructor(
-        public readonly command: T
-    ){}
-}
