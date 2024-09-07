79fdf6c0ab5d4847c78160e2a80af02eeaf1eab0
Can <yamahasirossi@gmail.com>
2024-09-08 00:34:22 +0300

    deleted

--- write-side/src/Core/Services/Sagas/CheckoutSagas.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { ICommand, Saga } from "@nestjs/cqrs";
-import {Observable, map} from 'rxjs'
-import { ofType } from '@nestjs/cqrs';
-
-@Injectable()
-export class CheckoutSagas {
-}
