bf4e368e059a5900c5f5d889344412a93459e13b
Can <yamahasirossi@gmail.com>
2024-09-08 00:40:13 +0300

    message queue implemantation removed

--- write-side/src/Core/Services/Commands/CommandHandlers/AddAnItemToCartCommandHandler.ts
+++ write-side/src/Core/Services/Commands/CommandHandlers/AddAnItemToCartCommandHandler.ts
@@ -1,6 +1,5 @@
 import { CommandHandler, ICommandHandler, EventPublisher, EventBus } from '@nestjs/cqrs';
 import AddAnItemCommand from '../Command/AddAnItemCommand';
-import CheckoutRepository from '../../../Interfaces/CheckoutRepository';
 import { Inject } from '@nestjs/common';
 import Checkout from '../../../Models/Domain Models/Checkout/Checkout';
 import { IDomainModelFactoryContext } from '../../../Models/Factories/DomainModelFactoryContext';
@@ -8,18 +7,10 @@ import ConcreateCheckoutItemFactory from '../../../Models/Factories/CheckoutItem
 import CheckoutItemInterface from '../../../Models/Domain Models/Checkout/CheckoutItemInterface';
 import CheckoutItemConstructorParameters from 'src/Core/Models/Factories/CheckoutItem/CheckoutItemConstructorParameters';
 import CheckoutNotFound from 'src/Core/Exceptions/CheckoutNotFound';
-import MessageQueue from '../../../Interfaces/MessageQueue';
 import { randomUUID } from 'crypto';
 import SuccessResult from 'src/Core/Models/Result/SuccsessResult';
-import { DataSource } from 'typeorm';
 import ICheckoutRepositoryFactory from 'src/Core/Interfaces/ICheckoutRepositoryFactory';
-import ITransactionManagerFactory from 'src/Core/Interfaces/ITransactionManagerFactory';
-interface ProductResponse {
-    header: string,
-    price: number,
-    hasError: boolean
-    errorMessage?: string; 
-}
+
 @CommandHandler(AddAnItemCommand)
 export default class AddAnItemToCartCommadHandler implements ICommandHandler<AddAnItemCommand> {
     constructor(
@@ -27,8 +18,6 @@ export default class AddAnItemToCartCommadHandler implements ICommandHandler<Add
         private readonly checkoutRepositoryFactory: ICheckoutRepositoryFactory,
         @Inject("DomainModelFactoryContext") 
         private readonly domainModelFactoryContext: IDomainModelFactoryContext,
-        @Inject("MessageQueue")
-        private readonly messageQueue: MessageQueue,
         private readonly eventPublisher: EventPublisher
     ){}
     async execute(command: AddAnItemCommand): Promise<any> {
@@ -40,12 +29,6 @@ export default class AddAnItemToCartCommadHandler implements ICommandHandler<Add
             //if checkout cancelled it will throw exception
             checkoutDomainModel.isCheckoutCancelled()
             
-            await this.messageQueue.publishMessage("find-one-product-by-uuid", command.productUuid)
-            const productResponseFromProductService = await this.messageQueue.getResponseFromQueue<ProductResponse>("one-product-founded")
-            
-            if(productResponseFromProductService.hasError) {
-                throw new Error(productResponseFromProductService.errorMessage)
-            }
     
             const checkoutItem = this.domainModelFactoryContext
                                                         .setFactoryMethod(ConcreateCheckoutItemFactory.name)
@@ -55,8 +38,8 @@ export default class AddAnItemToCartCommadHandler implements ICommandHandler<Add
                                                             checkoutItemUuid: command.checkoutItemUuid ?? randomUUID(),
                                                             checkoutUuid: command.checkoutUuid,
                                                             createdAt: new Date,
-                                                            productBasePrice:productResponseFromProductService.price,
-                                                            productHeader:productResponseFromProductService.header,
+                                                            productBasePrice:command.productBasePrice,
+                                                            productHeader:command.productHeader,
                                                             productQuantity: command.quantity,
                                                             productUuid: command.productUuid,
                                                             updatedAt: new Date
