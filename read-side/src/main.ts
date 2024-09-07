03b9fc450fec5682f81a3743622075530d1379c7
Can <yamahasirossi@gmail.com>
2024-09-08 00:35:56 +0300

    port number assign as number

--- read-side/src/main.ts
+++ read-side/src/main.ts
@@ -6,6 +6,7 @@ import { Transport } from '@nestjs/microservices';
 async function bootstrap() {
   
   dotenv.config()
+  
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.APP_PORT);
   
@@ -13,7 +14,7 @@ async function bootstrap() {
       transport: Transport.REDIS,
       options: {
         host: process.env.MESSAGE_QUEUE_HOST,
-        port: process.env.MESSAGE_QUEUE_PORT,
+        port: new Number(process.env.MESSAGE_QUEUE_PORT).valueOf(),
     }
   }
   const projectionListener = await NestFactory.createMicroservice(ProjectionModule, queueTransportObject)
