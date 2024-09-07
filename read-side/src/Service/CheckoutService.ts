ef7e17d720a7d8b614eee6db4863fb479d7bc80a
Can <yamahasirossi@gmail.com>
2024-09-08 00:36:39 +0300

    function input replaced with dto

--- read-side/src/Service/CheckoutService.ts
+++ read-side/src/Service/CheckoutService.ts
@@ -1,3 +1,4 @@
+import FindAnCheckoutDto from '../Controller/DTO/FindAnCheckoutDto';
 export default interface CheckoutService {
-    findAnCheckoutByUuidAndCustomerUuid(checkoutUuid:string, customerUuid:string)
+    findAnCheckoutByUuidAndCustomerUuid(findAnCheckoutDto:FindAnCheckoutDto)
 }
