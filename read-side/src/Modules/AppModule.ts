a9ded43c661bc5d47eca4793792456a50a5db393
Can <yamahasirossi@gmail.com>
2024-09-07 21:03:34 +0300

    added some modules

--- read-side/src/Modules/AppModule.ts
+++ read-side/src/Modules/AppModule.ts
@@ -1,15 +1,22 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import JwtStrategy from 'src/Auth/JwtAuthPassport';
 import CheckoutReadController from 'src/Controller/CheckoutReadController';
 import CheckoutServiceImpl from 'src/Service/CheckoutServiceImpl';
 
 @Module({
-  imports: [],
+  imports: [
+    JwtModule.register({secret: process.env.JWT_SECRET_TOKEN}),
+    PassportModule
+  ],
   controllers: [CheckoutReadController],
   providers: [
     {
         provide: "CheckoutService",
         useClass: CheckoutServiceImpl
-    }
+    },
+    JwtStrategy
   ],
 })
 export class AppModule {}
