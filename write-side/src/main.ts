2718892df96d7d6e6be95e9ad9e84169834abdf6
Can <yamahasirossi@gmail.com>
2024-09-08 00:40:29 +0300

    Update main.ts

--- write-side/src/main.ts
+++ write-side/src/main.ts
@@ -1,20 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import {Transport, MicroserviceOptions} from "@nestjs/microservices"
 import 'reflect-metadata';
-import CheckoutMicroserviceModule from './Application/Modules/CheckoutMicroserviceModule/CheckoutMicroserviceModule';
+import * as dotenv from "dotenv"
 import CheckoutClientModule from './Application/Modules/CheckoutClientModule/CheckoutClientModule';
 async function bootstrap() {
+  dotenv.config()
+  
   const app = await NestFactory.create(CheckoutClientModule);
-  await app.listen(3000);
-
-  const app2 = await NestFactory.createMicroservice<MicroserviceOptions>(
-    CheckoutMicroserviceModule, {
-    transport: Transport.REDIS,
-    options: {
-      host: 'localhost',
-      port: 6379,
-    },
-  });
-  await app2.listen()
+  await app.listen(process.env.APP_PORT);
+  
 }
 bootstrap();
