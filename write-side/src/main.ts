d1b3149224c1e9f6283145f3a4d7ef77838afb85
Can <yamahasirossi@gmail.com>
2024-09-09 19:58:45 +0300

    removed dotenv

--- write-side/src/main.ts
+++ write-side/src/main.ts
@@ -1,9 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import 'reflect-metadata';
-import * as dotenv from "dotenv"
 import CheckoutClientModule from './Application/Modules/CheckoutClientModule/CheckoutClientModule';
 async function bootstrap() {
-  dotenv.config()
   
   const app = await NestFactory.create(CheckoutClientModule);
   await app.listen(process.env.APP_PORT);
