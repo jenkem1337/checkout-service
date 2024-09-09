16700ef96968face2d8f34b77f46670efa9b0de1
Can <yamahasirossi@gmail.com>
2024-09-09 14:56:24 +0300

    catch exception with rxjs

--- read-side/src/Controller/CheckoutReadController.ts
+++ read-side/src/Controller/CheckoutReadController.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Inject, Param, Request, UseGuards } from "@nestjs/common";
+import { Controller, Get, Inject, NotFoundException, Param, Request, UseFilters, UseGuards, UseInterceptors } from "@nestjs/common";
 import CheckoutService from "src/Service/CheckoutService";
 import FindAnCheckoutDto from "./DTO/FindAnCheckoutDto";
 import { JwtAuthGuard } from "src/Auth/JwtAuthGuard";
+import { catchError, from, throwError } from "rxjs";
 
 @Controller("/api/v1/checkout")
 export default class CheckoutReadController {
@@ -14,8 +15,13 @@ export default class CheckoutReadController {
     @UseGuards(JwtAuthGuard)
     @Get("/:checkout_uuid")
     async getAnCheckoutByUuidAndCustomerUuid(@Request() req:any, @Param("checkout_uuid") checkoutUuid: string){
-      return await this.checkoutService.findAnCheckoutByUuidAndCustomerUuid(
-        new FindAnCheckoutDto(checkoutUuid, req.user.customerUUID)
-      )
+      return from(
+        this.checkoutService.findAnCheckoutByUuidAndCustomerUuid(
+          new FindAnCheckoutDto(checkoutUuid, req.user.customerUUID),
+        ),
+      ).pipe(
+          catchError((error) => 
+                    throwError(
+                        () => new NotFoundException(`${error.message}`))))
     }
 }
