e34243dc5965bb640df7b8b0f01df21dded1ae80
Can <yamahasirossi@gmail.com>
2024-09-09 14:59:16 +0300

    added type checkoutReadRepository property

--- read-side/src/Service/CheckoutServiceImpl.ts
+++ read-side/src/Service/CheckoutServiceImpl.ts
@@ -2,13 +2,14 @@ import { Inject, Injectable } from "@nestjs/common";
 import CheckoutService from "./CheckoutService";
 import CheckoutNotFound from '../Exceptions/CheckoutNotFound';
 import FindAnCheckoutDto from "src/Controller/DTO/FindAnCheckoutDto";
+import QueryableCheckoutReadRepository from "src/Repository/QueryableCheckoutReadRepository";
 
 @Injectable()
 export default class CheckoutServiceImpl implements CheckoutService {
     
     constructor(
         @Inject("CheckoutReadRepository")
-        private readonly checkoutReadRepository
+        private readonly checkoutReadRepository: QueryableCheckoutReadRepository
     ){}
     
     async findAnCheckoutByUuidAndCustomerUuid(dto: FindAnCheckoutDto) {
