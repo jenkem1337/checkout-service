bdf4fb18a11836b78219af91c003bf6920aca50a
Can <yamahasirossi@gmail.com>
2024-09-12 17:26:54 +0300

    added IdempotentMessageRepository and RedisModule

--- read-side/src/Modules/ProjectionModule.ts
+++ read-side/src/Modules/ProjectionModule.ts
@@ -2,13 +2,21 @@ import { Module } from "@nestjs/common";
 import CheckoutProjection from "src/Projection/CheckoutProjection";
 import CheckoutReadRepositoryImpl from '../Repository/CheckoutReadRepositoryImpl';
 import MongoModule from "./MongoModule";
+import IdempotentMessageRepositoryImpl from "src/Repository/IdempotentMessageRepositoryImpl";
+import RedisModule from "./RedisModule";
 
 @Module({
     controllers: [CheckoutProjection],
-    providers:[{
-        provide: "CheckoutReadRepository",
-        useClass: CheckoutReadRepositoryImpl
-    }],
-    imports: [MongoModule]
+    providers:[
+        {
+            provide: "CheckoutReadRepository",
+            useClass: CheckoutReadRepositoryImpl
+        },
+        {
+            provide: "IdempotentMessageRepository",
+            useClass: IdempotentMessageRepositoryImpl
+        }
+    ],
+    imports: [MongoModule, RedisModule]
 })
 export default class ProjectionModule{}
