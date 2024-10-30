2659a07a1aedbbeb1d7be037fb0b2b1ad475f45c
Can <yamahasirossi@gmail.com>
2024-10-31 00:06:10 +0300

    Update IBAN.ts

--- write-side/src/Core/Models/Domain Models/Checkout/PeymentContext/IBAN.ts
+++ write-side/src/Core/Models/Domain Models/Checkout/PeymentContext/IBAN.ts
@@ -1,2 +1,17 @@
 import PaymentStrategy from './PeymentStrategy';
-export default class IBAN implements PaymentStrategy{}
+interface IBANDetail {
+    no:string
+}
+export default class IBAN implements PaymentStrategy<IBANDetail>{
+    private no:string
+    
+    setDetail(detail: IBANDetail) {
+        if(!detail.no) {
+            throw new Error("IBAN no is null or not setted !")
+        }
+        this.no = detail.no
+    }
+    getNo(){
+        return this.no
+    }
+}
