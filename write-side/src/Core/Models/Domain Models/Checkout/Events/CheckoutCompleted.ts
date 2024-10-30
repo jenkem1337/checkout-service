9dc67b3fbd9dda87cabff08f7e5ca653bf61fc70
Can <yamahasirossi@gmail.com>
2024-10-31 00:06:01 +0300

    Update CheckoutCompleted.ts

--- write-side/src/Core/Models/Domain Models/Checkout/Events/CheckoutCompleted.ts
+++ write-side/src/Core/Models/Domain Models/Checkout/Events/CheckoutCompleted.ts
@@ -1,10 +1,17 @@
-import { IEvent } from '@nestjs/cqrs';
-import CheckoutID from '../../../ValueObjects/CheckoutID';
 import DomainEvent from './DomainEvent';
+import Address from 'src/Core/Models/ValueObjects/Address';
+import CreditCart from '../PeymentContext/CreditCard';
+import IBAN from '../PeymentContext/IBAN';
+import PaymentStrategy from '../PeymentContext/PeymentStrategy';
 export default class CheckoutCompleted extends DomainEvent {
-    public checkoutUuid: CheckoutID
-    constructor(uuid:CheckoutID) {
+    
+    constructor(
+        readonly checkoutUuid:string,
+        readonly customerUuid:string,
+        readonly peymentMethod:string,
+        readonly peymentDetail: PaymentStrategy<object>,
+        readonly orderAddress:Address,
+    ) {
         super()
-        this.checkoutUuid = this.checkoutUuid
     }
 }
