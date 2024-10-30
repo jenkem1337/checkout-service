8f3431336177d7503ce8076aad56cb7a13e8b617
Can <yamahasirossi@gmail.com>
2024-10-31 00:06:08 +0300

    Update CreditCard.ts

--- write-side/src/Core/Models/Domain Models/Checkout/PeymentContext/CreditCard.ts
+++ write-side/src/Core/Models/Domain Models/Checkout/PeymentContext/CreditCard.ts
@@ -1,2 +1,30 @@
+import DeleteAnItemDto from 'src/Application/Controller/DTOs/DeleteAnItemDto';
 import PaymentStrategy from './PeymentStrategy';
-export default class CreditCart implements PaymentStrategy{}
+interface CreditCartDetail {
+    number:string,
+    expirationDate:string,
+    cvc:string,
+    owner:string
+}
+export default class CreditCart implements PaymentStrategy<CreditCartDetail>{
+    private number:string
+    private expirationDate:string
+    private cvc:string
+    private owner:string
+    
+    setDetail(detail: CreditCartDetail) {
+        if(!detail.number) throw new Error("Cart number is null or not setted")
+        if(!detail.expirationDate) throw new Error("Cart expiration date is null or not setted")
+        if(!detail.cvc) throw new Error("Cart cvc number is null or not setted")
+        if(!detail.owner) throw new Error("Cart owner is null or not setted")
+        
+        this.number = detail.number
+        this.expirationDate = detail.expirationDate
+        this.cvc = detail.cvc
+        this.owner = detail.owner
+    }
+    getNumber = () => this.number
+    getExpirationDate = () => this.expirationDate
+    getCVC = () => this.cvc
+    getOwner = () => this.owner
+}
